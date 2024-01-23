04983769e730b3e63c2673313f0d866f218f4d7d
André Ramos <115324164+andremvsramos@users.noreply.github.com>
2024-01-23 17:50:20 +0000

    Add files via upload

--- srcs/requirements/wordpress/tools/startup.sh
+++ srcs/requirements/wordpress/tools/startup.sh
@@ -1,10 +1,11 @@
 # Config php.ini
+#tail -f /dev/null
 
-sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/fpm/php.ini
-sed -i "s/upload_max_filesize = .*/upload_max_filesize = 128M/" /etc/php/fpm/php.ini
-sed -i "s/zlib.output_compression = .*/zlib.output_compression = on/" /etc/php/fpm/php.ini
-sed -i "s/max_execution_time = .*/max_execution_time = 18000/" /etc/php/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 256M/" /etc/php/7.4/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 128M/" /etc/php/7.4/fpm/php.ini
+sed -i "s/zlib.output_compression = .*/zlib.output_compression = on/" /etc/php/7.4/fpm/php.ini
+sed -i "s/max_execution_time = .*/cdmax_execution_time = 18000/" /etc/php/7.4/fpm/php.ini
 
-service php-fpm start
-service php-fpm stop
-php-fpm -F -R
+service php7.4-fpm start
+service php7.4-fpm stop
+php-fpm7.4 -F -R
